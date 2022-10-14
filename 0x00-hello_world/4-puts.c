423b54887cb7bf4c3d1c6fb23c981878f6b54fbf
Obinna Robinson <obinnarobinson1@gmail.com>
2022-10-14 15:54:57 -0700

    some needed hot fix

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,11 +3,11 @@
  * main - Prints "Programming is like building a multilingual
  *                puzzle, followed by a new line.
  *  
- *  Return: Always 0.
+ * Return: Always 0.
  */
 int main(void)
 {
 		puts("\"Programming is like building a multilingual puzzle");
 
-			return (0);
+		return (0);
 }
