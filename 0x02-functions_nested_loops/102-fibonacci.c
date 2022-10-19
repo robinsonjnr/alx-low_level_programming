4501c0ccec9f03c1ad5a8a0e731621ad3db37da8
Obinna Robinson <obinnarobinson1@gmail.com>
2022-10-19 13:17:48 -0700

    functions and loops in C

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - prints the first fib num begining with 1 and 2
+ * num should be seperated by a comma and followed by space
+ * Return: 0
+ */
+int main(void)
+{
+	int count;
+	unsigned long fibi = 0, fib = 1, sum;
+
+	for (count = 0; count < 50; count++)
+	{
+		sum = fib1 + fib2;
+		printf("%lu", sum);
+
+		fib1 = fib2;
+		fib2 = sum;
+
+		if (count == 49)
+			printf("\n");
+		else
+			printf(", ");
+	}
+
+	return (0);
+}
