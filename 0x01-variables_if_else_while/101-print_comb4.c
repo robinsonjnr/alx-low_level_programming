7bb96e4327a185914ddf26d75b954c111126180f
Obinna Robinson <obinnarobinson1@gmail.com>
2022-10-19 00:54:12 -0700

    if, else, and while of variables

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 /**
- * main - program entry point
- * Description: program prints all possible combinations of three digits
- * Return :0 always
+ * main - print all possible combinations of three diff digits,
+ * in ascending order, seperated by comma and followed by space
+ * Return: 0 always
  */
 int main(void)
 {
 	int digit1, digit2, digit3;
 
-	for (digit1 = 0; digit < 8; digit1++)
+	for (digit1 = 0; digit1 < 8; digit1++)
 	{
 		for (digit2 = digit1 + 1; digit2 < 9; digit2++)
 		{
