674c6e41ead1c84ac8f4e4f9de42edabfdcb3dd8
Obinna Robinson <obinnarobinson1@gmail.com>
2022-10-20 05:33:09 -0700

    functions and loops in C

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,13 +1,15 @@
 #include "main.h"
-
+#include <stdio.h>
 /**
  * add - Adds two integers.
  * @x: the first integer
  * @y: the second integer
  *
- * Return: the result of the addition.
+ * Return: 0
  */
 int add(int x, int y)
 {
-	return (x + y);
+	int sum = x + y
+		printf("%d\n", sum);
+	return (0);
 }
