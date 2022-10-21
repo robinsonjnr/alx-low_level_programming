b9e05356f7a094045b3081f573775dbe2468b108
Obinna Robinson <obinnarobinson1@gmail.com>
2022-10-21 12:47:16 -0700

    debugging tasks

---
+++ 0x03-debugging/2-largest_numbers.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * largest_number - prints the largest number of three imputed integers
+ * @a: let this be the variable for the value of the first integer
+ * @b: be the variable for the value of the second integer
+ * @c: be the variable for the value of the last integer
+ * Return: largest value
+ */
+int largest_number(int a, int b, int c)
+{
+	int largest;
+
+	if (a >= b && a >= c)
+	{
+		largest = a;
+	}
+	else if (b >= a && b >= C)
+	{
+		largest = b
+	}
+	else
+		largest = c
+	}
+
+	return (largest);
+}
+
