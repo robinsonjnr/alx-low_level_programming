90b9b98480e70f221989ec578d5a373b32c45479
Obinna Robinson <obinnarobinson1@gmail.com>
2022-10-22 02:12:01 -0700

    more functions and loops

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -11,7 +11,7 @@ void print_most_numbers(void)
 		for (numbers = 0; numbers <= 9; numbers++)
 		{
 			if (numbers != 2 && numbers != 4)
-				_putchar(numbers);
+				_putchar((numbers % 10) + '0');
 		}
 		_putchar('\n');
 
